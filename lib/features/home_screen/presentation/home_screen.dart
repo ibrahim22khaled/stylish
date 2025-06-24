7aa3a09270de99dbcfa2af6927acb5537714e46f
Ibrahim Khaled <ibra17kh@gmail.com>
2025-06-24 15:59:52 +0300

    edit the appbar

--- lib/features/home_screen/presentation/home_screen.dart
+++ lib/features/home_screen/presentation/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
+import 'package:stylish/core/widgets/custom_text.dart';
 import 'package:stylish/features/home_screen/presentation/screens/home_screen_details.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -14,10 +16,19 @@ class HomeScreen extends StatelessWidget {
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            SvgPicture.asset("assets/icons/menu.svg"),
-            Image.asset("assets/images/homscreenlogo.jpg",
-            width: 111.7,
-            height: 31.03,),
+            SvgPicture.asset("assets/icons/menu_icon.svg"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.asset("assets/images/homescreenlogo.jpg",
+                width: 38,
+                height: 31,),
+                const Gap(5),
+                CustomText(text: "Stylish", fontWeight: FontWeight.bold, fontSize:18, textColor: Color(0xff4392F9),fontFamily: 'LibreCaslon',)
+
+              ],
+            ),
+            
             Image.asset("assets/images/useravatar.jpg",
             width: 40,
             height: 40,),
