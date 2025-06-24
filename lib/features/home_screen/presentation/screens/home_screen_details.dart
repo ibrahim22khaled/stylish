8d251b232f5cbaf4fea918cb27e17b451f62ce04
Ibrahim Khaled <ibra17kh@gmail.com>
2025-06-24 15:20:28 +0300

    Finish category card UI

--- lib/features/home_screen/presentation/screens/home_screen_details.dart
+++ lib/features/home_screen/presentation/screens/home_screen_details.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
-import 'package:stylish/core/utils/size_config.dart';
-import 'package:stylish/core/widgets/custom_text.dart';
 import 'package:stylish/features/home_screen/presentation/widgets/all_featured_widget.dart';
+import 'package:stylish/features/home_screen/presentation/widgets/category_card.dart';
 import 'package:stylish/features/home_screen/presentation/widgets/search_textfield.dart';
 
 class HomeScreenDetails extends StatefulWidget {
@@ -21,31 +20,8 @@ class _HomeScreenDetailsState extends State<HomeScreenDetails> {
         SliverToBoxAdapter(child: SearchTextfield()),
         SliverToBoxAdapter(child: const Gap(10)),
         SliverToBoxAdapter(child: AllFeaturedWidget()),
-                SliverToBoxAdapter(child: const Gap(5)),
-
-        SliverToBoxAdapter(
-          child:
-          Padding(
-            padding: EdgeInsets.only(left: SizeConfig.defaultSize! * 2),
-            child: Container(
-              height: 87,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5),
-                color: Colors.white,
-              ),
-              child:Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Column(children: [
-                  CircleAvatar(
-                    radius: 28,
-                    child: Image.asset("assets/images/beauty.jpg"),
-                  ),
-                  CustomText(text: "Beauty", fontWeight: FontWeight.w100, fontSize: 10, textColor: Colors.black)
-                ],),
-              )
-            ),
-          )
-        )
+        SliverToBoxAdapter(child: const Gap(5)),
+        SliverToBoxAdapter(child: CategoryCard()),
       ],
     );
   }
--- lib/features/home_screen/presentation/widgets/category_card.dart
+++ lib/features/home_screen/presentation/widgets/category_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:stylish/core/utils/size_config.dart';
 import 'package:stylish/core/widgets/custom_text.dart';
 
@@ -7,29 +8,64 @@ class CategoryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-            padding: EdgeInsets.only(left: SizeConfig.defaultSize! * 2),
-            child: Container(
-              height: 87,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5),
-                color: Colors.white,
-              ),
-              child:Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Row(
-                  children: [
-                    Column(children: [
-                      CircleAvatar(
-                        radius: 28,
-                        child: Image.asset("assets/images/beauty.jpg"),
-                      ),
-                      CustomText(text: "Beauty", fontWeight: FontWeight.w100, fontSize: 10, textColor: Colors.black)
-                    ],),
-                  ],
+    return  Padding(
+      padding:  EdgeInsets.only(left:SizeConfig.defaultSize! * 2),
+      child: Container(
+                height: 87,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.white,
                 ),
-              )
-            ),
-          );
+                child:Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal:10),
+                      child: Row(
+                        children: [
+                          CategoryCardItem(image: "assets/images/beauty.jpg", cardTitle: "Beauty"),
+                          const Gap(20),
+                          CategoryCardItem(image: "assets/images/fashion.jpg", cardTitle: "Fashion"),
+                          const Gap(20),
+                          CategoryCardItem(image: "assets/images/kids.jpg", cardTitle: "Kids"),
+                          const Gap(20),
+                          CategoryCardItem(image: "assets/images/men.jpg", cardTitle: "Men"),
+                          const Gap(20),
+                          CategoryCardItem(image: "assets/images/women.jpg", cardTitle: "Women"),
+                          const Gap(20),
+                          CategoryCardItem(image: "assets/images/gifts.jpg", cardTitle: "Gifts"),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              ),
+    );
+         
+  }
+}
+class CategoryCardItem extends StatelessWidget {
+  const CategoryCardItem({super.key, required this.image, required this.cardTitle});
+  final String image;
+  final String cardTitle;
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 28,
+          child: Image.asset(image),
+        ),
+        CustomText(
+          text: cardTitle,
+          fontWeight: FontWeight.w100,
+          fontSize: 10,
+          textColor: Colors.black,
+        ),
+      ],
+    );
   }
 }
