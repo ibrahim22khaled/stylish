326f349e3e96879ff44ab53ceaff8df5f893325d
Ibrahim Khaled <ibra17kh@gmail.com>
2025-07-02 19:52:05 +0300

    domian layer > product entity

---
+++ lib/features/home_screen/domain/entities/product_entity.dart
@@ -0,0 +1,15 @@
+class ProductEntity {
+  final String name;
+  final String description;
+  final num price;
+  final num rate;
+  final int reviews;
+
+  ProductEntity(
+     this.name,
+     this.description,
+     this.price,
+     this.rate,
+     this.reviews,
+  );
+}
