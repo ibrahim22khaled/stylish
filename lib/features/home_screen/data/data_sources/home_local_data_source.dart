79cafc56341ca33e2d86070ae3db160911f365fb
Ibrahim Khaled <ibra17kh@gmail.com>
2025-07-05 06:37:55 +0300

    create home local data source class

---
+++ lib/features/home_screen/data/data_sources/home_local_data_source.dart
@@ -0,0 +1,12 @@
+import 'package:stylish/features/home_screen/domain/entities/product_entity.dart';
+
+abstract class HomeLocalDataSource {
+  List<ProductEntity>getProducts(); // it's not a future function beacause the data here ir's already cashed!
+}
+class HomeLocalDataSourceImpl extends HomeLocalDataSource{
+  @override
+  List<ProductEntity> getProducts() {
+    // TODO: implement getProducts
+    throw UnimplementedError();
+  }
+}
--- lib/features/home_screen/data/data_sources/home_remote_data_source.dart
+++ lib/features/home_screen/data/data_sources/home_remote_data_source.dart
@@ -27,3 +27,27 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
   }
   
 }
+
+// here below thre whole class before refactoring "getProductsList"
+
+
+/*abstract class HomeRemoteDataSource {
+  Future<List<ProductEntity>> getProducts();   // here I don't care about Failure because Failure is managed by HomeRepo class in domain layer..
+                                               // here I care about getting data using API service.
+}
+class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
+  final ApiService apiService;
+  HomeRemoteDataSourceImpl(this.apiService);
+  @override
+  Future<List<ProductEntity>> getProducts() async{
+  var data =await apiService.get(endPoint: 'products');  // data is a list of maps... It depends on the API data shape.
+
+  List<ProductEntity>products=[];
+  for (var productMap in data){   // productMap is the map of {String and dynamic} in the whole list which I've called it "data"
+    products.add(ProductModel.fromJson(productMap));  // Here we parse the data from API to Product entity
+  }
+    return products;
+
+  }
+  
+}*/
