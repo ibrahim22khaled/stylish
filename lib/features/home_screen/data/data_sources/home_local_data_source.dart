ef4218d29e333edc17ef0faa6eaa78e67bda208a
Ibrahim Khaled <ibra17kh@gmail.com>
2025-07-10 12:42:19 +0300

    add hive

--- lib/features/home_screen/data/data_sources/home_local_data_source.dart
+++ lib/features/home_screen/data/data_sources/home_local_data_source.dart
@@ -1,11 +1,11 @@
 import 'package:stylish/features/home_screen/domain/entities/product_entity.dart';
 
 abstract class HomeLocalDataSource {
-  List<ProductEntity>getProducts(); // it's not a future function beacause the data here ir's already cashed!
+  List<ProductEntity>getProducts(); // it's not a future function beacause the data here it's already cashed!
 }
 class HomeLocalDataSourceImpl extends HomeLocalDataSource{
   @override
-  List<ProductEntity> getProducts() {
+  List<ProductEntity> getProducts() { 
     // TODO: implement getProducts
     throw UnimplementedError();
   }
