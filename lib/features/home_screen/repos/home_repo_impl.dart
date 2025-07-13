0e4903578b39ee3f1c65241fa07e52235fb9c8fc
Ibrahim Khaled <ibra17kh@gmail.com>
2025-07-13 12:25:06 +0300

    finsh home repo impl class implementation

--- lib/features/home_screen/repos/home_repo_impl.dart
+++ lib/features/home_screen/repos/home_repo_impl.dart
@@ -1,13 +1,32 @@
 import 'package:dartz/dartz.dart';
 import 'package:stylish/core/errors/failure.dart';
+import 'package:stylish/features/home_screen/data/data_sources/home_local_data_source.dart';
+import 'package:stylish/features/home_screen/data/data_sources/home_remote_data_source.dart';
 import 'package:stylish/features/home_screen/domain/entities/product_entity.dart';
 import 'package:stylish/features/home_screen/domain/repos/home_repo.dart';
 
-class HomeRepoImpl extends HomeRepo{
-  @override
-  Future<Either<Failure, List<ProductEntity>>> getProducts() {
-    // TODO: implement getProducts
-    throw UnimplementedError();
-  }
+class HomeRepoImpl extends HomeRepo {
+  /// this calss to collect home & remote data source
 
+  final HomeRemoteDataSource homeRemoteDataSource;
+  final HomeLocalDataSource homeLocalDataSource;
+
+  HomeRepoImpl({
+    required this.homeRemoteDataSource,
+    required this.homeLocalDataSource,
+  });
+  @override
+  Future<Either<Failure, List<ProductEntity>>> getProducts() async {
+    try {
+      List<ProductEntity>products;
+      products = homeLocalDataSource.getProducts();
+      if (products.isNotEmpty) {
+        return right(products);
+      }
+      products = await homeRemoteDataSource.getProducts();
+      return right(products);
+    } catch (e) {
+      return left(Failure());
+    }
+  }
 }
