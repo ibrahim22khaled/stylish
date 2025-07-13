1e5a1cec4605aad1431a0b052e0e032b4a8365e9
Ibrahim Khaled <ibra17kh@gmail.com>
2025-07-13 12:04:05 +0300

    add box of product entitiy in home local data source to get data cached in device memory

--- lib/features/home_screen/data/data_sources/home_local_data_source.dart
+++ lib/features/home_screen/data/data_sources/home_local_data_source.dart
@@ -1,3 +1,5 @@
+import 'package:hive/hive.dart';
+import 'package:stylish/core/utils/constants.dart';
 import 'package:stylish/features/home_screen/domain/entities/product_entity.dart';
 
 abstract class HomeLocalDataSource {
@@ -6,7 +8,8 @@ abstract class HomeLocalDataSource {
 class HomeLocalDataSourceImpl extends HomeLocalDataSource{
   @override
   List<ProductEntity> getProducts() { 
-    // TODO: implement getProducts
-    throw UnimplementedError();
-  }
+    var box = Hive.box<ProductEntity>(KFeaturedBox);
+    return box.values.toList();
+    /// it gets data cached in my device memory from the SaveProductsData fun from remote data source by Hive
+}
 }
