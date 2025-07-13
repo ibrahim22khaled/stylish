1dc21fc87abecb3e3bdfd75ada1cb49c3bd41a20
Ibrahim Khaled <ibra17kh@gmail.com>
2025-07-13 09:36:09 +0300

    generate product entity adapter

--- lib/features/home_screen/domain/entities/product_entity.dart
+++ lib/features/home_screen/domain/entities/product_entity.dart
@@ -1,9 +1,23 @@
+import 'package:hive/hive.dart';
+part 'product_entity.g.dart';
+ // to generate product type adapterflutter packages pub run build_runner build
+
+
+// run this prompt in terminal "  flutter packages pub run build_runner build  "
+
+@HiveType(typeId:0)
 class ProductEntity {
+  @HiveField(0)
   final int productId;
+  @HiveField(1)
   final String productName;
+  @HiveField(2)
   final String productDescription;
+  @HiveField(3)
   final num productPrice;
+  @HiveField(4)
   final num productRate;
+  @HiveField(5)
   final int productReviews;
 
   ProductEntity({
@@ -15,3 +29,4 @@ class ProductEntity {
     required this.productReviews,
   });
 }
+///////////////////
