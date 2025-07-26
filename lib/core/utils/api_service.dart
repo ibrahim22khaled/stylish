652009815e482154fd20a249400090fa93686df4
Ibrahim Khaled <ibra17kh@gmail.com>
2025-07-26 06:53:10 +0300

    update service api file to cope with the retrieved data from API

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -4,10 +4,18 @@ class ApiService {
   final Dio _dio;
   ApiService(this._dio);
 
-  final baseUrl= "https://fakestoreapi.com/";
-  Future <List<Map<String,dynamic>>> get({required String endPoint}) async{
-   var response = await _dio.get('$baseUrl$endPoint');
-   return response.data;
-  }
+  final String baseUrl = "https://fakestoreapi.com/";
 
+  Future<List<Map<String, dynamic>>> get({required String endPoint}) async {
+    try {
+      final response = await _dio.get('$baseUrl$endPoint');
+      if (response.data is List) {
+        return (response.data as List).cast<Map<String, dynamic>>();
+      } else {
+        throw Exception('API response is not a list');
+      }
+    } catch (e) {
+      throw Exception('Failed to fetch data from API: $e');
+    }
+  }
 }
