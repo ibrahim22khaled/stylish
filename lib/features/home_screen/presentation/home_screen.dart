daa6c164f54fecb8f5f99091b95b8cbea7253893
Ibrahim Khaled <ibra17kh@gmail.com>
2025-07-26 06:49:28 +0300

    add product entity fields in screen

--- lib/features/home_screen/presentation/home_screen.dart
+++ lib/features/home_screen/presentation/home_screen.dart
@@ -8,6 +8,8 @@ import 'package:stylish/features/home_screen/presentation/screens/search_screen.
 import 'package:stylish/features/home_screen/presentation/screens/setting_screen.dart';
 import 'package:stylish/features/home_screen/presentation/screens/wish_list_screen.dart';
 
+
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -34,84 +36,85 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xffF9F9F9),
-      appBar: AppBar(
+    return  Scaffold(
         backgroundColor: Color(0xffF9F9F9),
-        shadowColor: Color(0xffF9F9F9),
-        surfaceTintColor: Color(0xffF9F9F9),
-        title: CustomAppBar(),
-      ),
-      body: _screens[_selectedIndex],
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: Container(
-        height: 58,
-        width: 58,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black12,
-              blurRadius: 8,
-              offset: Offset(0, 4),
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Color(0xffF9F9F9),
+          shadowColor: Color(0xffF9F9F9),
+          surfaceTintColor: Color(0xffF9F9F9),
+          title: CustomAppBar(),
         ),
-        child: FloatingActionButton(
-          onPressed: () => _onNavTapped(2), // Cart tab index
-          backgroundColor: _selectedIndex == 2 ? kMainColor: Colors.white,
-          elevation: 8,
-          child: Icon(
-            Icons.shopping_cart,
-            color: _selectedIndex == 2? Colors.white : Colors.black,
-            size: 30,
-          ),
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        notchMargin: 8,
-        color: Colors.white,
-        child: SizedBox(
-          height: 64,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              // Home
-              _buildNavItem(
-                icon: Icons.home_outlined,
-                label: 'Home',
-                isActive: _selectedIndex == 0,
-                onTap: () => _onNavTapped(0),
-              ),
-              // Wishlist
-              _buildNavItem(
-                icon: Icons.favorite_border,
-                label: 'Wishlist',
-                isActive: _selectedIndex == 1,
-                onTap: () => _onNavTapped(1),
-              ),
-              // Spacer for FAB
-              const Gap(60),
-              // Search
-              _buildNavItem(
-                icon: Icons.search,
-                label: 'Search',
-                isActive: _selectedIndex == 3,
-                onTap: () => _onNavTapped(3),
-              ),
-              // Setting
-              _buildNavItem(
-                icon: Icons.settings,
-                label: 'Setting',
-                isActive: _selectedIndex == 4,
-                onTap: () => _onNavTapped(4),
+        body: _screens[_selectedIndex],
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: Container(
+          height: 58,
+          width: 58,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 8,
+                offset: Offset(0, 4),
               ),
             ],
           ),
+          child: FloatingActionButton(
+            onPressed: () => _onNavTapped(2), // Cart tab index
+            backgroundColor: _selectedIndex == 2 ? kMainColor : Colors.white,
+            elevation: 8,
+            child: Icon(
+              Icons.shopping_cart,
+              color: _selectedIndex == 2 ? Colors.white : Colors.black,
+              size: 30,
+            ),
+          ),
         ),
-      ),
-    );
+        bottomNavigationBar: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          notchMargin: 8,
+          color: Colors.white,
+          child: SizedBox(
+            height: 64,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                // Home
+                _buildNavItem(
+                  icon: Icons.home_outlined,
+                  label: 'Home',
+                  isActive: _selectedIndex == 0,
+                  onTap: () => _onNavTapped(0),
+                ),
+                // Wishlist
+                _buildNavItem(
+                  icon: Icons.favorite_border,
+                  label: 'Wishlist',
+                  isActive: _selectedIndex == 1,
+                  onTap: () => _onNavTapped(1),
+                ),
+                // Spacer for FAB
+                const Gap(60),
+                // Search
+                _buildNavItem(
+                  icon: Icons.search,
+                  label: 'Search',
+                  isActive: _selectedIndex == 3,
+                  onTap: () => _onNavTapped(3),
+                ),
+                // Setting
+                _buildNavItem(
+                  icon: Icons.settings,
+                  label: 'Setting',
+                  isActive: _selectedIndex == 4,
+                  onTap: () => _onNavTapped(4),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    
   }
 
   Widget _buildNavItem({
