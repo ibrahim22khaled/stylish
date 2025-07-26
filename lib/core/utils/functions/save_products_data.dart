a474164826d16b125f77e4ac2e25de927dc29000
Ibrahim Khaled <ibra17kh@gmail.com>
2025-07-26 06:52:19 +0300

    correct mistake about hive type

--- lib/core/utils/functions/save_products_data.dart
+++ lib/core/utils/functions/save_products_data.dart
@@ -2,6 +2,6 @@ import 'package:hive/hive.dart';
 import 'package:stylish/features/home_screen/domain/entities/product_entity.dart';
 
 void SaveProductsData(List<ProductEntity> products, String boxName) {
-     var box = Hive.box(boxName);
+     var box = Hive.box<ProductEntity>(boxName);
     box.addAll(products);
   }
