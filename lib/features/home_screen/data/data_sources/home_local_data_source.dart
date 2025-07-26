75b27e8f98bd108b74dd080f9766e340aa74396a
Ibrahim Khaled <ibra17kh@gmail.com>
2025-07-26 06:50:55 +0300

    add some style to make the comment readable

--- lib/features/home_screen/data/data_sources/home_local_data_source.dart
+++ lib/features/home_screen/data/data_sources/home_local_data_source.dart
@@ -3,11 +3,12 @@ import 'package:stylish/core/utils/constants.dart';
 import 'package:stylish/features/home_screen/domain/entities/product_entity.dart';
 
 abstract class HomeLocalDataSource {
-  List<ProductEntity>getProducts(); // it's not a future function beacause the data here it's already cashed!
+  List<ProductEntity>getProducts(); 
+  // it's not a future function beacause the data here it's already cashed!
 }
 class HomeLocalDataSourceImpl extends HomeLocalDataSource{
   @override
-  List<ProductEntity> getProducts() { 
+ List<ProductEntity> getProducts() { 
     var box = Hive.box<ProductEntity>(KHomeBox);
     return box.values.toList();
     /// it gets data cached in my device memory from the SaveProductsData fun from remote data source by Hive
