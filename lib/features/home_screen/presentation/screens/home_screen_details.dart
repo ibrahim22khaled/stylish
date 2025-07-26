131e81320cfdbc2a14bc36d3d29335a95519996d
Ibrahim Khaled <ibra17kh@gmail.com>
2025-07-26 06:47:34 +0300

    add some comments temporarly to refactor the screen

--- lib/features/home_screen/presentation/screens/home_screen_details.dart
+++ lib/features/home_screen/presentation/screens/home_screen_details.dart
@@ -3,7 +3,6 @@ import 'package:gap/gap.dart';
 import 'package:stylish/features/home_screen/presentation/widgets/all_featured_widget.dart';
 import 'package:stylish/features/home_screen/presentation/widgets/category_card.dart';
 import 'package:stylish/features/home_screen/presentation/widgets/offers_container.dart';
-import 'package:stylish/features/home_screen/presentation/widgets/product_card_list_view.dart';
 import 'package:stylish/features/home_screen/presentation/widgets/promo_carousel_screen.dart';
 import 'package:stylish/features/home_screen/presentation/widgets/search_textfield.dart';
 import 'package:stylish/features/home_screen/presentation/widgets/season_cover_offer.dart';
@@ -34,15 +33,15 @@ class _HomeScreenDetailsState extends State<HomeScreenDetails> {
           child: OffersContainer(containerTitle: "Deal of the Day", containerColor: Color(0xff4392F9), onTap: () {  },)
         ),
         SliverToBoxAdapter(child: const Gap(10)),
-        SliverToBoxAdapter(child: ProductCardListView()
-        ),
+        // SliverToBoxAdapter(child: ProductCardListView()
+        // ),
         SliverToBoxAdapter(child: const Gap(10)),
         SliverToBoxAdapter(
           child: OffersContainer(containerTitle: "Trending Products", containerColor: Color(0xffFD6E87), onTap: () { Navigator.of(context).pushNamed('trendingproductsscreen') ;},)
         ),
         SliverToBoxAdapter(child: const Gap(10)),
-        SliverToBoxAdapter(child: ProductCardListView()
-        ),
+        // SliverToBoxAdapter(child: ProductCardListView()
+        // ),
         SliverToBoxAdapter(child: const Gap(10)),
         SliverToBoxAdapter(child: SeasonCoverOffer()),
         SliverToBoxAdapter(child: const Gap(30)),
